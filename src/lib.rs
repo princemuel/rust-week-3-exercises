4235eb10be6b19011d52b4d561f763bb77874044
Prince Muel <68931805+princemuel@users.noreply.github.com>
2025-07-03 11:29:46 +0100

    test: make tests::test_outpoint_roundtrip pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 use std::fmt;
 use std::ops::Deref;
 
+use serde::de::Error as DeError;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Serialize, Deserialize)]
 pub struct CompactSize {
     pub value: u64,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum BitcoinError {
     InsufficientBytes,
     InvalidFormat,
@@ -19,14 +20,14 @@ impl CompactSize {
 
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = match self.value {
-            0x00..=0xfc => Vec::with_capacity(1),
+            ..=0xfc => Vec::with_capacity(1),
             0xfd..=0xffff => Vec::with_capacity(3),
             0x10000..=0xffffffff => Vec::with_capacity(5),
             _ => Vec::with_capacity(9),
         };
 
         match self.value {
-            0..=252 => bytes.push(self.value as u8),
+            ..=252 => bytes.push(self.value as u8),
             253..=65535 => {
                 bytes.push(0xfd);
                 bytes.extend_from_slice(&(self.value as u16).to_le_bytes());
@@ -47,19 +48,19 @@ impl CompactSize {
     pub fn from_bytes(bytes: &[u8]) -> Result<(Self, usize), BitcoinError> {
         match bytes {
             [] => Err(BitcoinError::InsufficientBytes),
-            [first @ 0x00..=0xfc, ..] => Ok((Self::new(*first as u64), 1)),
+            [uno @ ..=0xfc, ..] => Ok((Self::new(*uno as u64), 1)),
             [0xfd, rest @ ..] => {
-                let slice = rest.get(0..2).ok_or(BitcoinError::InsufficientBytes)?;
+                let slice = rest.get(..2).ok_or(BitcoinError::InsufficientBytes)?;
                 let value = u16::from_le_bytes(slice.try_into().unwrap()) as u64;
                 Ok((Self::new(value), 3))
             },
             [0xfe, rest @ ..] => {
-                let slice = rest.get(0..4).ok_or(BitcoinError::InsufficientBytes)?;
+                let slice = rest.get(..4).ok_or(BitcoinError::InsufficientBytes)?;
                 let value = u32::from_le_bytes(slice.try_into().unwrap()) as u64;
                 Ok((Self::new(value), 5))
             },
             [0xff, rest @ ..] => {
-                let slice = rest.get(0..8).ok_or(BitcoinError::InsufficientBytes)?;
+                let slice = rest.get(..8).ok_or(BitcoinError::InsufficientBytes)?;
                 let value = u64::from_le_bytes(slice.try_into().unwrap());
                 Ok((Self::new(value), 9))
             },
@@ -67,20 +68,35 @@ impl CompactSize {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Txid(pub [u8; 32]);
 
 impl Serialize for Txid {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: serde::Serializer {
-        todo!()
+    fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut bytes = self.0;
+        bytes.reverse();
+        s.serialize_str(&hex::encode(bytes))
     }
 }
 
 impl<'de> Deserialize<'de> for Txid {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where D: serde::Deserializer<'de> {
-        todo!()
+    fn deserialize<D>(d: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let hex_string = String::deserialize(d)?;
+
+        let decoded =
+            hex::decode(&hex_string).map_err(|_| DeError::custom("Invalid hex string"))?;
+
+        let bytes = decoded
+            .try_into()
+            .map_err(|_| DeError::custom("Txid must be exactly 32 bytes"))?;
+
+        Ok(Txid(bytes))
     }
 }
 
@@ -91,11 +107,28 @@ pub struct OutPoint {
 }
 
 impl OutPoint {
-    pub fn new(txid: [u8; 32], vout: u32) -> Self { todo!() }
+    pub fn new(txid: [u8; 32], vout: u32) -> Self {
+        Self {
+            vout,
+            txid: Txid(txid),
+        }
+    }
 
-    pub fn to_bytes(&self) -> Vec<u8> { todo!() }
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(36);
+        bytes.extend_from_slice(&self.txid.0);
+        bytes.extend_from_slice(&self.vout.to_le_bytes());
+        bytes
+    }
 
-    pub fn from_bytes(bytes: &[u8]) -> Result<(Self, usize), BitcoinError> { todo!() }
+    pub fn from_bytes(bytes: &[u8]) -> Result<(Self, usize), BitcoinError> {
+        if bytes.len() < 36 {
+            return Err(BitcoinError::InsufficientBytes);
+        }
+        let txid = (&bytes[..32]).try_into().unwrap();
+        let vout = u32::from_le_bytes((&bytes[32..36]).try_into().unwrap());
+        Ok((Self::new(txid, vout), 36))
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
